741597a58377e541a92f88f295ffbf322758da58
Brandon Walker <bcw58@cornell.edu>
2018-05-09 03:04:08 -0400

    Added evolution with population of one agent

--- evolution.py
+++ evolution.py
@@ -32,21 +32,33 @@ class Simulation:
         if self.draw:
             self.view = View(self.world)
 
+    def initialize_teams(self):
+        """Initialize list of teams."""
+        teams = []
+        for i in range(self.num_agents):
+            if i < self.num_agents/2:
+                teams.append(0)
+            else:
+                teams.append(1)
+        random.shuffle(teams)
+        return teams
+
     def initialize_agents(self, world, nn_weights_list=None):
         """Add agents to [self.world]."""
         # Precondition: [self.world] and [self.num_agents] must be defined
-        self.world.agents = {}
+        world.agents = {}
+        teams = self.initialize_teams()
         for i in range(self.num_agents):
             nn_weights = None
             if nn_weights_list:
                 nn_weights = nn_weights_list[i]
-            self.initialize_agent(world, i, nn_weights=nn_weights)
+            self.initialize_agent(world, i, teams[i], nn_weights=nn_weights)
 
-    def initialize_agent(self, world, i, nn_model=None, nn_weights=None):
+    def initialize_agent(self, world, i, team, nn_model=None, nn_weights=None):
         agent = Agent(i)
         agent.move = bool(random.getrandbits(1))
         agent.direction = Direction(random.randrange(4))
-        agent.team = random.getrandbits(1)
+        agent.team = team
         if nn_model:
             agent.model = nn_model
         else:
@@ -55,10 +67,10 @@ class Simulation:
                 agent.model.set_weights(nn_weights)
         x = random.randrange(world.width)
         y = random.randrange(world.height)
-        while (x, y) in self.world.agents:
+        while (x, y) in world.agents:
             x = random.randrange(world.width)
             y = random.randrange(world.height)
-        self.world.agents[(x, y)] = agent
+        world.agents[(x, y)] = agent
         return (x, y)
 
     def initialize_agent_nn(self):
@@ -76,13 +88,16 @@ class Simulation:
         model.add(Dense(output_vars, activation='sigmoid'))
         return model
 
-    def run(self, retain=0.2, random_select=0.05, mutate=0.01, mutpb=0.6):
-        """Run genetic algorithm."""
+    def run(self, world, one=False, retain=0.2, random_select=0.05, mutate=0.01, mutpb=0.6):
+        """Run genetic algorithm on [world]."""
         for generation in range(self.generation, self.num_generations):
             # Grade - [graded] is list of tuples ([score], model) - higher is better
-            self.run_world_simulation(generation)
+            if one:
+                self.run_world_simulation_one(generation)
+            else:
+                self.run_world_simulation(world, generation)
             print('fitness gen {}:'.format(generation))
-            graded = self.evaluate_fitnesses()
+            graded = self.evaluate_fitnesses(world, one)
             # print('{}'.format(graded))
             print('\t{}'.format(self.fitnesses))
             retain_length = int(len(graded)*retain)
@@ -99,7 +114,7 @@ class Simulation:
                     tools.mutation.mutShuffleIndexes(individual[0], mutpb)
             # Crossover parents to create children
             parents_len = len(parents)
-            desired_len = len(self.world.agents)
+            desired_len = len(world.agents)
             children = []
             while len(children) < desired_len:
                 maleIdx = random.randint(0, parents_len-1)
@@ -116,57 +131,80 @@ class Simulation:
             # Create new population
             parents.extend(children)
             nn_weights_list = [self.unflatten_model_weights(x) for x in parents]
-            self.initialize_agents(self.world, nn_weights_list=nn_weights_list)
+            self.initialize_agents(world, nn_weights_list=nn_weights_list)
 
-    def run_world_simulation(self, generation):
+    def run_world_simulation(self, world, generation, one_loc=None):
         """Run agents through world simulation."""
         draw_path = '{}gen{}/world'.format(self.path, generation)
+        if one_loc:
+            one_agent = self.world.agents[one_loc]
+            draw_path = '{}gen{}/agent-i{}/world'.format(self.path, generation, one_agent.private_id)
         draw_ext = '.png'
         if self.draw:
-            self.view.draw(self.world)
+            self.view.draw(world)
             self.view.save(draw_path+'0'+draw_ext)
-        print('max_time: {}'.format(self.max_time))
+        # print('max_time: {}'.format(self.max_time))
         for t in range(1, self.max_time):
-            self.update()
+            self.update(world)
             if self.draw:
                 self.view.save(draw_path+str(t)+draw_ext)
 
-    def evaluate_fitnesses(self):
+    def run_world_simulation_one(self, generation):
+        """Run agents through world simulations of only themselves."""
+        for loc in self.world.agents:
+            agent = self.world.agents[loc]
+            model = agent.model
+            # Compose world of agent at loc
+            world = World(self.world.width, self.world.height)
+            world.agents = {}
+            teams = self.initialize_teams()
+            for i in range(self.num_agents):
+                self.initialize_agent(world, i, teams[i], nn_model=model)
+            # Run simulation
+            self.run_world_simulation(world, generation, one_loc=loc)
+            # Calculate and save fitnesses in [agent]
+            agent.fitness = 0
+            for loc in world.agents:
+                agent.fitness += self.fitness(loc, world)
+
+    def evaluate_fitnesses(self, world, one=False):
         """Evaluate fitnesses to [self.fitnesses]."""
         graded = []
         self.fitnesses = []
-        for loc in self.world.agents:
-            agent = self.world.agents[loc]
+        for loc in world.agents:
+            agent = world.agents[loc]
             graded.append(
-                (self.fitness(loc, self.world),
+                (loc,
+                self.fitness(loc, world, one=one),
                 self.flatten_model_weights(agent.model.get_weights()))
             )
-        graded = sorted(graded, key=lambda x: x[0], reverse=True)   # Higher fitness at front
-        self.fitnesses = [x[0] for x in graded]
-        graded = [x[1] for x in graded]
+        graded = sorted(graded, key=lambda x: x[1], reverse=True)   # Higher fitness at front
+        self.fitnesses = [x[1] for x in graded]
+        self.private_ids = [world.agents[x[0]].private_id for x in graded]
+        graded = [x[2] for x in graded]
         return graded
 
-    def visualize(self):
+    def visualize(self, world, one=False):
         """Save visualization of generation."""
         draw = self.draw
         self.draw = True
-        self.view = View(self.world)
-        self.run_world_simulation(self.generation)
-        self.evaluate_fitnesses()
-        # Save fitness
-        generation_path = '{}gen{}/'.format(self.path, self.generation)
-        fitness_path = '{}fitness.csv'.format(generation_path)
-        with open(fitness_path, 'w') as f:
-            f.write(','.join(map(str, self.fitnesses)))
-        print('\t{}'.format(self.fitnesses))
+        self.view = View(world)
+        if one:
+            self.run_world_simulation_one(self.generation)
+        else:
+            self.run_world_simulation(world, self.generation)
+            self.evaluate_fitnesses(world)
+            # Save fitness
+            print('fitness gen {}:'.format(self.generation))
+            print('\t{}'.format(self.fitnesses))
         self.draw = draw
 
-    def update(self):
-        for loc in self.world.agents:
-            self.update_agent(self.world.agents[loc])
-        self.world.update()
+    def update(self, world):
+        for loc in world.agents:
+            self.update_agent(world.agents[loc])
+        world.update()
         if self.draw:
-            self.view.draw(self.world)
+            self.view.draw(world)
 
     def update_agent(self, agent):
         """Run agent neural network (model) and update its state."""
@@ -203,8 +241,10 @@ class Simulation:
         output['move'] = bool(y[2])
         return output
 
-    def fitness(self, loc, world):
+    def fitness(self, loc, world, one=False):
         """Return fitness of agent at [loc] higher is better than lower."""
+        if one:
+            return world.agents[loc].fitness
         fitness = 0
         agent = world.agents[loc]
         next_loc = world.next_loc(loc, agent.direction)
@@ -215,6 +255,7 @@ class Simulation:
                 fitness += 10
                 if loc == world.next_loc(next_loc, adjacent_agent.direction):
                     fitness += 5
+        world.agents[loc].fitness = fitness
         return fitness
 
     def flatten_model_weights(self, weights):
@@ -249,12 +290,16 @@ class Simulation:
             f.write(self.world.agents[list(self.world.agents.keys())[0]].model.to_json())
         # Save all agent genomes (weights)
         for loc in self.world.agents:
-            agent_path = '{}agent{}.hdf5'.format(generation_path, loc)
+            agent = self.world.agents[loc]
+            agent_path = '{}agent-i{}.hdf5'.format(generation_path, agent.private_id)
             self.world.agents[loc].model.save_weights(agent_path)
         # Save fitness
+        mean = np.mean(self.fitnesses)
         fitness_path = '{}fitness.csv'.format(generation_path)
         with open(fitness_path, 'w') as f:
-            f.write(','.join(map(str, self.fitnesses)))
+            f.write('private_id,{}'.format(','.join(map(str, self.private_ids))))
+            f.write('\nfitnesses,{}'.format(','.join(map(str, self.fitnesses))))
+            f.write('\nmean,{}'.format(mean))
 
     def load_generation(self, generation=-1):
         """Load generation data from [self.path]/gen[generation]/"""
@@ -268,18 +313,18 @@ class Simulation:
             self.generation = generation
         # Iterate over agents
         self.world.agents.clear()
+        teams = self.initialize_teams()
         model_path = '{}model.json'.format(generation_path)
-        i = 0
         for name in os.listdir(generation_path):
-            if ('agent' not in name):
+            if not ('agent' in name and 'hdf5' in name):
                 continue
             agent_path = '{}{}'.format(generation_path, name)
             print(agent_path)
+            private_id = int(name[name.find('i')+1:-5])
             with open(model_path, 'r') as f:
                 model =  model_from_json(f.read())
             model.load_weights(agent_path)
-            self.initialize_agent(self.world, i, nn_model=model)
-            i += 1
+            loc = self.initialize_agent(self.world, private_id, teams[private_id], nn_model=model)
 
     def __str__(self):
         return str(self.world)
@@ -298,10 +343,15 @@ def visualize_generation():
         generation = int(sys.argv[3])
     else:
         generation = -1
+    # argv[4] = one
+    if (len(sys.argv) > 4):
+        one = (sys.argv[4] == 'True')
+    else:
+        one = False
     path = 'out/' + project_name + '/'
     simulation = Simulation(path=path)
     simulation.load_generation(generation=generation)
-    simulation.visualize()
+    simulation.visualize(simulation.world, one=one)
 
 def continue_evolution():
     """Continue runing an evolution from specified generation."""
@@ -315,9 +365,14 @@ def continue_evolution():
         generation = int(sys.argv[3])
     else:
         generation = -1
-    # argv[4] = draw
+    # argv[4] = one
     if (len(sys.argv) > 4):
-        draw = (sys.argv[4] == 'True')
+        one = (sys.argv[4] == 'True')
+    else:
+        one = False
+    # argv[5] = draw
+    if (len(sys.argv) > 5):
+        draw = (sys.argv[5] == 'True')
     else:
         draw = False
     # Create simulation
@@ -325,7 +380,7 @@ def continue_evolution():
     simulation = Simulation(path=path, draw=draw)
     simulation.load_generation(generation=generation)
     simulation.generation += 1
-    simulation.run()
+    simulation.run(simulation.world, one=one)
 
 def run_evolution():
     """Create a simulation and run the evolution."""
@@ -334,21 +389,26 @@ def run_evolution():
         project_name = sys.argv[2]
     else:
         project_name = 'untitled'
-    # argv[3] = draw
+    # argv[3] = one
     if (len(sys.argv) > 3):
-        draw = (sys.argv[3] == 'True')
+        one = (sys.argv[3] == 'True')
+    else:
+        one = False
+    # argv[4] = draw
+    if (len(sys.argv) > 4):
+        draw = (sys.argv[4] == 'True')
     else:
         draw = False
     # Create simulation
     path = 'out/' + project_name + '/'
     simulation = Simulation(path=path, draw=draw)
-    simulation.run()
+    simulation.run(simulation.world, one=one)
 
 def main():
     # argv[1] = command (run | cont | vis)
-    # python3 evolution.py run [project_name] [draw]
-    # python3 evolution.py cont [project_name] [generation] [draw]
-    # python3 evolution.py vis [project_name] [generation]
+    # python3 evolution.py run [project_name] [one] [draw]
+    # python3 evolution.py cont [project_name] [generation] [one] [draw]
+    # python3 evolution.py vis [project_name] [generation] [one]
     if (len(sys.argv) > 1):
         command = sys.argv[1]
     else:
